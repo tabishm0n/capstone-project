093d7f417f450a32f1e318474b7907d0f8b0f621
tabishm0n <tabishmon@gmail.com>
2022-02-26 20:03:58 -0500

    Initialized Java Class - DAO and Servlet

--- FoodDeliveryApp/src/main/java/com/capstoneproject/dao/UserDao.java
+++ FoodDeliveryApp/src/main/java/com/capstoneproject/dao/UserDao.java
@@ -1,38 +1,29 @@
 package com.capstoneproject.dao;
 
+
 import java.sql.Connection;
 import java.util.Date;
 
 import com.capstoneproject.model.User;
 
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao {
 	private Connection con;
-	
 	public UserDao(Connection con) {
 		this.con = con;
 	}
-	
-	
-	public int registerUser(User user) throws ClassNotFoundException{
-		
-		String INSERT_USERS_SQL = "INSERT INTO user_table " + "(id,login,password,city,"
+	public int registerUser(User user) {
+		String INSERT_USERS_SQL = "INSERT INTO user_table " + "(login,password,city,"
 				+ "street_address,first_name,last_name,email,phone,user_type,payment,"
 				+"registeration_date) VALUES (?,?,?,?,?,?,?,?,?,?,?);";
 		int result = 0 ;
-		Class.forName("org.postgresql.Driver");
-		
-		try  (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/project","postgres",
-				"Rinnegan0")){
+		try  {
 			Date date = new Date();
 			java.sql.Date sqldate = new java.sql.Date(date.getTime());
-			//prepared statements 
-			PreparedStatement ps = connection.prepareStatement(INSERT_USERS_SQL);
-			
+			PreparedStatement ps = con.prepareStatement(INSERT_USERS_SQL);
 			ps.setString(1, user.getLogin());
 			ps.setString(2, user.getPassword());
 			ps.setString(3, user.getCity());
@@ -44,7 +35,6 @@ public class UserDao {
 			ps.setString(9, user.getUser_type());
 			ps.setString(10, user.getPayment());
 			ps.setDate(11, sqldate);
-			
 			System.out.println(ps);
 			result = ps.executeUpdate();
 		}catch (SQLException e) {
@@ -52,11 +42,9 @@ public class UserDao {
 		}
 		return result;
 	}
-	
 	private void printSQLException(SQLException ex) {
 		for (Throwable e: ex) {
 			if(e instanceof SQLException) {
-				
 				e.printStackTrace(System.err);
 				System.err.println("SQLState: "+ ((SQLException) e).getSQLState());
 				System.err.println("Error Code: "+ ((SQLException) e).getErrorCode());
@@ -69,19 +57,11 @@ public class UserDao {
 			}
 		}
 	}
-	public boolean verifyUser(User user) throws ClassNotFoundException{
-		//create SQL statement
+	public boolean verifyUser(User user) {
 		boolean result = false;
 		String READ_USERS_SQL ="Select * FROM user_table WHERE login=? AND password=?";
-		
-		Class.forName("org.postgresql.Driver");
-		
-		try  (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/project","tabishmon",
-				"Rinnegan0")){
-			
-			//prepared statements 
-			PreparedStatement ps = connection.prepareStatement(READ_USERS_SQL);
-			
+		try  {
+			PreparedStatement ps = con.prepareStatement(READ_USERS_SQL);
 			ps.setString(1, user.getLogin());
 			ps.setString(2, user.getPassword());
 			System.out.println(ps);
@@ -91,25 +71,17 @@ public class UserDao {
 		}
 		return result;
 	}
-	public int verifyType(User user) throws ClassNotFoundException{
-		//create SQL statement
+	public int verifyType(User user) {
+		
 		int result = 0;
 		String READ_TYPE_SQL ="Select user_type FROM user_table WHERE login=? AND password=?";
 		
-		Class.forName("org.postgresql.Driver");
-		
-		try  (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/project","tabishmon",
-				"Rinnegan0")){
-			
-			//prepared statements 
-			PreparedStatement ps1 = connection.prepareStatement(READ_TYPE_SQL);
-			
+		try {
+			PreparedStatement ps1 = con.prepareStatement(READ_TYPE_SQL);
 			ps1.setString(1, user.getLogin());
 			ps1.setString(2, user.getPassword());
-			
 			ResultSet rs1 = ps1.executeQuery();
 			while(rs1.next()) {
-				
 					String type = rs1.getString("user_type");
 				if(type.equals("Customer")) {
 					result = 1;
--- FoodDeliveryApp/src/main/java/com/capstoneproject/servlet/UserLogin.java
+++ FoodDeliveryApp/src/main/java/com/capstoneproject/servlet/UserLogin.java
@@ -1,5 +1,70 @@
 package com.capstoneproject.servlet;
 
-public class UserLogin {
+import java.io.IOException;
+import java.sql.SQLException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import com.capstoneproject.dao.UserDao;
+import com.capstoneproject.model.User;
+import com.capstoneproject.connection.DbCon;
+
+@WebServlet("/Login")
+
+public class UserLogin extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	private UserDao usDao;
+	
+	public void init() {
+		try {
+			usDao = new UserDao(DbCon.getConnection());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		
+		String login = request.getParameter("login");
+		String password = request.getParameter("password");
+		String city = request.getParameter("city");
+		String street_address = request.getParameter("street_address");
+		String first_name = request.getParameter("first_name");
+		String last_name = request.getParameter("last_name");
+		String email = request.getParameter("email");
+		String phone = request.getParameter("phone");
+		String user_type = request.getParameter("user_type"); 
+		String payment = request.getParameter("payment");
+		
+		User us = new User();
+		us.setLogin(login);
+		us.setPassword(password);
+		us.setCity(city);
+		us.setStreet_address(street_address);
+		us.setFirst_name(first_name);
+		us.setLast_name(last_name);
+		us.setEmail(email);
+		us.setPhone(phone);
+		us.setUser_type(user_type);
+		us.setPayment(payment);
+		
+		try {
+			usDao.registerUser(us);
+			}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		response.sendRedirect("index.jsp");
+		}
+		
 }
