1af1199a76ae757032e38df0f6e30d55b0ae6884
tabishm0n <tabishmon@gmail.com>
2022-03-13 23:55:17 -0400

    Added Insert commands to DB

--- FoodDeliveryApp/database/sql-database.sql
+++ FoodDeliveryApp/database/sql-database.sql
@@ -1,21 +1,4 @@
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
-
-
 CREATE TABLE public.category (
     category_id bigint NOT NULL,
     category_name character varying(50) NOT NULL
@@ -330,71 +313,6 @@ ALTER TABLE ONLY public.user_table ALTER COLUMN id SET DEFAULT nextval('public.u
 
 
 
-COPY public.category (category_id, category_name) FROM stdin;
-1	Appetizer
-2	Main Course
-3	Sides
-4	Dessert
-5	Beverages
-\.
-
-
-
-
-COPY public.ingredients (ingredients_id, ingredients, item_name, menuitem_id) FROM stdin;
-1	Butter	Butter Chicken	1
-2	Onion	Butter Chicken	1
-3	Garlic	Butter Chicken	1
-4	Tomato Sauce	Butter Chicken	1
-5	Cream	Butter Chicken	1
-6	Chicken	Butter Chicken	1
-7	Salt	Butter Chicken	1
-8	Oil	Butter Chicken	1
-\.
-
-
-
-COPY public.menu_item (menuitem_id, item_name, description, price, active, restaurant_id, category) FROM stdin;
-1	Butter Chicken	Delicious Indian Curry with tender Chicken	30.5	t	1	2
-2	Biryani	Exquisite Hyderabadi Biryani served with Chicken	42.5	t	1	2
-4	Samosa	Indian Snack filled with flavored masehed Potatoes	10.5	t	1	1
-3	Chinese Noodles	Hand-made Noodles served in hot Chicken broth	18.0	t	2	2
-\.
-
-
-
-
-COPY public.restaurant (restaurant_id, city, street_address, operational, description, restaurant_name, user_id) FROM stdin;
-1	Toronto	Carlson St.	t	Small Family run Restaurant	Indian Curry	3
-2	Sudbury	Eglinton St.	t	Authentic Chinese Cuisine	China Town	4
-3	Toronto	Thorn St.	t	Country Side beautiful fresh Steak!	BBQ Corner	5
-4	Brampton	Wellesley St.	t	World Famous Burger fast-food chain	MC Chicken	6
-\.
-
-
-
-
-COPY public.status_list (status_id, status_name) FROM stdin;
-1	Order Created
-2	Order Prepared
-3	Order out for Delivery
-4	Order Delivered
-\.
-
-
-COPY public.user_table (id, login, password, city, street_address, first_name, last_name, email, phone, user_type, payment, registeration_date) FROM stdin;
-1	tabishmon	Admin123	Toronto	Wellesley St	Tabish	Osman	tabishmon@gmail.com	3453453455	Admin	1231231233	2022-02-21 23:10:45.297816
-4	lee	123	Sudbury	Chinatown	Lee	Chaolan	lee123@gmail.com	2345643987	Restaurant	239847984	2022-03-01 19:54:38.037444
-3	bharath	123	Toronto	Yonge St.	Bharath	Kumar	bharathkumar@gmail.com	9872342349	Restaurant	1236548153	2022-03-01 19:24:33.224157
-5	chris	123	Toronto	Jarl St.	Chris	Evans	chris.evans@gmail.com	2345723987	Restaurant	12739084	2022-03-01 20:05:04.359832
-6	tom	123	Toronto	Bayview St.	Tommy	Wiseau	itstommy@gmail.com	692354987	Restaurant	984321964	2022-03-01 20:06:46.655692
-7	naseeh	123	naseeh	Manchuria	Naseeh	Uddin	naseeh@gmail.com	92385692	Customer	346346664	2022-03-12 00:00:00
-2	owais	123	Toronto	George St	Owais	Bhai	owais123@gmail.com	0982340982	Customer	0980982348	2022-02-22 00:00:00
-8	tauseef	123	Tasueef	Khammam	Tauseef	Nihal	tauseef@gmail.com	576597568	Delivery	68760876087	2022-03-13 00:00:00
-9	asad	123	Nalgonda	Chabutra	Asaduddin	Owaisi	asad123@gmail.com	4895734987	Delivery	3464574574	2022-03-13 00:00:00
-\.
-
-
 SELECT pg_catalog.setval('public.category_category_id_seq', 1, false);
 
 
@@ -542,3 +460,61 @@ ALTER TABLE ONLY public.orders
     ADD CONSTRAINT orders_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.user_table(id);
 
 
+
+INSERT INTO public.category VALUES (1, 'Appetizer');
+INSERT INTO public.category VALUES (2, 'Main Course');
+INSERT INTO public.category VALUES (3, 'Sides');
+INSERT INTO public.category VALUES (4, 'Dessert');
+INSERT INTO public.category VALUES (5, 'Beverages');
+
+
+
+INSERT INTO public.deliverer VALUES (2, 0, 9);
+INSERT INTO public.deliverer VALUES (1, 0, 8);
+INSERT INTO public.deliverer VALUES (3, 0, 10);
+
+
+
+INSERT INTO public.ingredients VALUES (1, 'Butter', 'Butter Chicken', 1);
+INSERT INTO public.ingredients VALUES (2, 'Onion', 'Butter Chicken', 1);
+INSERT INTO public.ingredients VALUES (3, 'Garlic', 'Butter Chicken', 1);
+INSERT INTO public.ingredients VALUES (4, 'Tomato Sauce', 'Butter Chicken', 1);
+INSERT INTO public.ingredients VALUES (5, 'Cream', 'Butter Chicken', 1);
+INSERT INTO public.ingredients VALUES (6, 'Chicken', 'Butter Chicken', 1);
+INSERT INTO public.ingredients VALUES (7, 'Salt', 'Butter Chicken', 1);
+INSERT INTO public.ingredients VALUES (8, 'Oil', 'Butter Chicken', 1);
+
+
+
+INSERT INTO public.menu_item VALUES (1, 'Butter Chicken', 'Delicious Indian Curry with tender Chicken', 30.5, true, 1, 2);
+INSERT INTO public.menu_item VALUES (2, 'Biryani', 'Exquisite Hyderabadi Biryani served with Chicken', 42.5, true, 1, 2);
+INSERT INTO public.menu_item VALUES (4, 'Samosa', 'Indian Snack filled with flavored masehed Potatoes', 10.5, true, 1, 1);
+INSERT INTO public.menu_item VALUES (3, 'Chinese Noodles', 'Hand-made Noodles served in hot Chicken broth', 18.0, true, 2, 2);
+
+
+
+
+INSERT INTO public.restaurant VALUES (1, 'Toronto', 'Carlson St.', true, 'Small Family run Restaurant', 'Indian Curry', 3);
+INSERT INTO public.restaurant VALUES (2, 'Sudbury', 'Eglinton St.', true, 'Authentic Chinese Cuisine', 'China Town', 4);
+INSERT INTO public.restaurant VALUES (3, 'Toronto', 'Thorn St.', true, 'Country Side beautiful fresh Steak!', 'BBQ Corner', 5);
+INSERT INTO public.restaurant VALUES (4, 'Brampton', 'Wellesley St.', true, 'World Famous Burger fast-food chain', 'MC Chicken', 6);
+
+
+
+INSERT INTO public.status_list VALUES (1, 'Order Created');
+INSERT INTO public.status_list VALUES (2, 'Order Prepared');
+INSERT INTO public.status_list VALUES (3, 'Order out for Delivery');
+INSERT INTO public.status_list VALUES (4, 'Order Delivered');
+
+
+
+INSERT INTO public.user_table VALUES (1, 'tabishmon', 'Admin123', 'Toronto', 'Wellesley St', 'Tabish', 'Osman', 'tabishmon@gmail.com', '3453453455', 'Admin', '1231231233', '2022-02-21 23:10:45.297816');
+INSERT INTO public.user_table VALUES (4, 'lee', '123', 'Sudbury', 'Chinatown', 'Lee', 'Chaolan', 'lee123@gmail.com', '2345643987', 'Restaurant', '239847984', '2022-03-01 19:54:38.037444');
+INSERT INTO public.user_table VALUES (3, 'bharath', '123', 'Toronto', 'Yonge St.', 'Bharath', 'Kumar', 'bharathkumar@gmail.com', '9872342349', 'Restaurant', '1236548153', '2022-03-01 19:24:33.224157');
+INSERT INTO public.user_table VALUES (5, 'chris', '123', 'Toronto', 'Jarl St.', 'Chris', 'Evans', 'chris.evans@gmail.com', '2345723987', 'Restaurant', '12739084', '2022-03-01 20:05:04.359832');
+INSERT INTO public.user_table VALUES (6, 'tom', '123', 'Toronto', 'Bayview St.', 'Tommy', 'Wiseau', 'itstommy@gmail.com', '692354987', 'Restaurant', '984321964', '2022-03-01 20:06:46.655692');
+INSERT INTO public.user_table VALUES (7, 'naseeh', '123', 'naseeh', 'Manchuria', 'Naseeh', 'Uddin', 'naseeh@gmail.com', '92385692', 'Customer', '346346664', '2022-03-12 00:00:00');
+INSERT INTO public.user_table VALUES (2, 'owais', '123', 'Toronto', 'George St', 'Owais', 'Bhai', 'owais123@gmail.com', '0982340982', 'Customer', '0980982348', '2022-02-22 00:00:00');
+INSERT INTO public.user_table VALUES (8, 'tauseef', '123', 'Tasueef', 'Khammam', 'Tauseef', 'Nihal', 'tauseef@gmail.com', '576597568', 'Delivery', '68760876087', '2022-03-13 00:00:00');
+INSERT INTO public.user_table VALUES (9, 'asad', '123', 'Nalgonda', 'Chabutra', 'Asaduddin', 'Owaisi', 'asad123@gmail.com', '4895734987', 'Delivery', '3464574574', '2022-03-13 00:00:00');
+INSERT INTO public.user_table VALUES (10, 'jerry', '123', 'Toronto', 'Whale St', 'Jerry', 'Junior', 'jerry123@gmail.com', '234234499', 'Delivery', '23454634', '2022-03-13 00:00:00');
